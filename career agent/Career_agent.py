95664a0efdd16034725f990188c68697c4c91b6e
Ramisha Tariq <ramishatariq13@gmail.com>
2026-05-28 21:01:16 +0500

    update data

--- career agent/Career_agent.py
+++ career agent/Career_agent.py
@@ -6,6 +6,7 @@ def get_career_roadmap(field: str) -> str:
         "software engineer": "Learn programming languages, data structures, algorithms, web dev, Github, Projects.",
         "data scientist": "Learn statistics, data analysis, machine learning, Python, ML, Pandas, and real-world datasets.",
         "graphic designer": "Learn Figma, Photoshop, Adobe Creative Suite, typography, color theory, Projects.",
-        "ai": "Learn AI concepts, machine learning, Deep learning, Python, Transformers, PyTorch, and AI tools."
+        "full stack developer": "Learn front-end (HTML, CSS, JavaScript) and back-end (Node.js, databases) development, Git, and projects.",
+        "ai engineer": "Learn AI concepts, machine learning, Deep learning, Python, Transformers, PyTorch, and AI tools."
     }
     return maps.get(field.lower(), "No roadmap available for this field.")
